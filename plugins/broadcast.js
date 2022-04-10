fd94998440e0cea08bcbbde2fb4dc3bdaab0ca2e
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-10 08:48:32 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, text }) => {
   let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n\n'+'*「 BROADCAST ALL 」*')
+  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n\n'+'*「 BROADCAST DanzBot 」*')
   for (let id of chats) conn.copyNForward(id, content, true)
-  conn.reply(m.chat, `*SUKSES MENGIRIM PESAN KE ${chats.length} CHAT*`, m)
+  conn.reply(m.chat, `*Sukses Kirim Ke ${chats.length} Chat, Done Ya Ngan Danz*`, m)
 }
 handler.help = ['broadcast','bc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
