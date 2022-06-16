bb10744c11729cddc04945677b78d2f21a909cae
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-16 11:54:42 +0700

    Update daftar.js

--- plugins/daftar.js
+++ plugins/daftar.js
@@ -40,7 +40,7 @@ let handler = async function (m, { text, usedPrefix }) {
 │ *Kalian ingin unreg*
 ╰━━━━━━━━━━━━┈ ❋ཻུ۪۪⸙
 
-*「 NayLaBotz 」*
+*「 Yahya17Botz 」*
 `.trim())
 }
 handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
