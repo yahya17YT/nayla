f0597ab8768c3f08c8835308ef88c5a6742d970d
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-16 11:34:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
-global.owner = ['6288270863279'] // masuk in no lu sebagai owner
-global.mods = ['6288270863279'] // masuk in no moderator ny
+global.owner = ['6285784715971'] // masuk in no lu sebagai owner
+global.mods = ['6285784715971'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
-  // name: 'https://website'
+  // name: 'https://youtube.com/channel/UCYZ3s2hcv-3uYuR-BuO6wqg'
   nrtm: 'https://nurutomo.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
   nzcha: 'http://nzcha-apii.herokuapp.com',
@@ -30,15 +30,15 @@ global.APIKeys = { // Apikey lu di sini
 }
 
 // Sticker Watermark
-global.packname = 'DanzBot'
-global.author = 'DanzBot'
+global.packname = 'Yahya17Botz'
+global.author = 'Yahya17Botx'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
 global.wait = '*WAIT SABAR🥴*'
 global.error = '*ERROR BANH MAAP🗿*'
 global.botwm = 'DanzBot' // watermark bot
-global.oname = 'Danz Owner' // nama owner nya
+global.oname = 'Yahya Owner' // nama owner nya
 global.thum = 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 
 let chalk = require('chalk')
