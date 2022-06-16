7f6535e4ae768c03413e4c9898e357109051873a
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-16 12:05:35 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,7 +2,9 @@ let handler = async m => m.reply(`
 *LIST GC BOT*
 
 *GROUP 1*
-https://chat.whatsapp.com/JgFOfu182li7kG6dlKcwzD
+https://chat.whatsapp.com/Kqio6vednj6EJITkjvUsdG
+*GRUP 2*
+https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['gcbot']
 handler.tags = ['info']
